793221e5475173c9d0b3c4c7cf6138044ec6e1fe
Diego <dpeletei@thoughtworks.com>
2015-05-09 15:54:40 +0200

    Diego four

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        SparkapplicationImpl sparkapplication = new SparkapplicationImpl();
+
+        setPort(Integer.parseInt(System.getenv("PORT")));
+
         get("/hello", (req, res) -> "Hello World");
 
         get("/hello/:name", (req, res) -> {
---
+++ src/main/java/SparkapplicationImpl.java
@@ -0,0 +1,8 @@
+import spark.servlet.SparkApplication;
+
+public class SparkapplicationImpl implements SparkApplication {
+    @Override
+    public void init() {
+
+    }
+}
