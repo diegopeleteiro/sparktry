805472bdeddf4b32ff75eae192ec5473c6f11783
Diego <dpeletei@thoughtworks.com>
2015-05-09 16:00:43 +0200

    removed port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
 
-        setPort(Integer.parseInt(System.getenv("PORT")));
+//        setPort(Integer.parseInt(System.getenv("PORT")));
 
         get("/hello", (req, res) -> "Hello World");
 
